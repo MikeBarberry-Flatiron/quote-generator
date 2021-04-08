034a86d86e28fb1ba874e1e3a1c1b93c0739aa6f
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-04-07 20:07:46 -0700

    update seeder and add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# MenuGenerator
+Rails API with Vanilla JS frontend 
+
+# Running Locally 
+Make sure you have ruby, rails, bundler and postgres installed <br>
+Git clone this repo into a fresh directory <br>
+Cd server and `bundle install` <br>
+Run `rails db:create`, `rails db:migrate`, and `rails db:seed` to get your database up and running. <br>
+Start the rails server <br>
+Cd to the client directory and run index.html in your browser.
+
+# Contributing
+Open a pull request to contribute
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,4 +1,4 @@
-#require 'faker'
+require 'faker'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,8 +7,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# (1..3).each do |food|
-#    dish = Faker::Food.dish 
-#    description = Faker::Food.description
-#    Food.create(dish: dish, description: description, restaurant_id: 4)
-#end 
+(1..4) each do |restaurant|
+    name = Faker::Restaurant.name
+    Restaurant.create(name: name)
+end 
+
+(1..2).each do |food|
+    dish = Faker::Food.dish 
+    description = Faker::Food.description
+    Food.create(dish: dish, description: description, restaurant_id: 1)
+end 
+
+(1..2).each do |food|
+    dish = Faker::Food.dish 
+    description = Faker::Food.description
+    Food.create(dish: dish, description: description, restaurant_id: 2)
+end 
+
+(1..2).each do |food|
+    dish = Faker::Food.dish 
+    description = Faker::Food.description
+    Food.create(dish: dish, description: description, restaurant_id: 3)
+end 
+
+(1..2).each do |food|
+    dish = Faker::Food.dish 
+    description = Faker::Food.description
+    Food.create(dish: dish, description: description, restaurant_id: 4)
+end 
+ 
